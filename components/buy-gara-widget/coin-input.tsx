777d0ac03ebd82a5824c4ac80a6d1ba8bc60f08c
Your Name <lustykjakub@gmail.com>
2024-12-02 13:43:26 +0100

    fucking work locale piece of shit

--- components/buy-gara-widget/coin-input.tsx
+++ components/buy-gara-widget/coin-input.tsx
@@ -1,21 +1,29 @@
-"use client"
+"use client";
 
-import { forwardRef } from "react"
-import { cn } from "@/lib/utils"
-import Image from "next/image"
+import { forwardRef } from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const CoinInput = forwardRef<
   HTMLInputElement,
   {
-    name: string
-    coin: string
-    placeholder?: string
-    type: string
-    readOnly?: boolean
-    className?: string
-    showIcon?: boolean
+    name: string;
+    coin: string;
+    placeholder?: string;
+    type: string;
+    readOnly?: boolean;
+    className?: string;
+    showIcon?: boolean;
   } & React.InputHTMLAttributes<HTMLInputElement>
 >(({ coin, className, showIcon = true, error, ...rest }, ref) => {
+  const formatToLocale = (value: string | number | undefined) => {
+    if (value === undefined) return "";
+    return new Intl.NumberFormat("en", {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 4,
+    }).format(Number(value));
+  };
+
   return (
     <div className="flex flex-col">
       <div
@@ -25,20 +33,34 @@ export const CoinInput = forwardRef<
       >
         <input
           ref={ref}
+          lang="en"
           min="0"
           step="0.0001"
-          // max={"100000"}
-          className={cn("flex-1 bg-transparent font-heading text-lg text-gary-blue outline-none", className)}
+          className={cn(
+            "flex-1 bg-transparent font-heading text-lg text-gary-blue outline-none",
+            className
+          )}
+          onChange={(e) => {
+            const formattedValue = formatToLocale(e.target.value);
+            e.target.value = formattedValue;
+          }}
           {...rest}
         />
         {showIcon ? (
           <div className="flex flex-row items-center justify-end gap-4 font-heading text-base font-semibold text-gary-blue">
-            <Image src={`/icons/coins/${coin?.toLowerCase()}.png`} alt={coin} width={32} height={32} />
+            <Image
+              src={`/icons/coins/${coin?.toLowerCase()}.png`}
+              alt={coin}
+              width={32}
+              height={32}
+            />
             <span className="w-[46px]">{coin}</span>
           </div>
         ) : null}
       </div>
-      {error ? <span className="ml-4 mt-1 text-sm text-red-500">{error}</span> : null}
+      {error ? (
+        <span className="ml-4 mt-1 text-sm text-red-500">{error}</span>
+      ) : null}
     </div>
-  )
-})
+  );
+});
