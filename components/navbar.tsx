45627995cfb7746358e991ac3d53c0836423f248
Your Name <lustykjakub@gmail.com>
2025-01-31 16:11:26 +0100

    audit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -134,6 +134,77 @@ export const NavBar = () => {
           </AnchorLink>
         </div>
       </div>
+      
+      {/* Burger Menu Button */}
+      <div className="fixed right-4 top-12 z-[9999]">
+        <button className="focus:outline-none lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-[#061022]">
+            {menuOpen ? (
+              <Image src="/icons/close.svg" alt="Close" width={24} height={24} />
+            ) : (
+              <Image src="/icons/burger-menu.svg" alt="Menu" width={24} height={24} />
+            )}
+          </div>
+        </button>
+      </div>
+
+      {menuOpen && (
+        <div className="fixed inset-0 z-[9998] flex h-full w-full flex-col items-center justify-center overflow-y-auto bg-black bg-opacity-90 pb-10 pt-10">
+          <div className="my-8">
+            <Image src="/logo.png" alt="Gary" width={75} height={75} />
+          </div>
+
+          <ul className="flex flex-col gap-4 text-center">
+            {links.map((link) => (
+              <li key={link.name}>
+                {link.href ? (
+                  <a
+                    href={link.href}
+                    target="_blank"
+                    className="text-lg font-bold text-white"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    {link.name}
+                  </a>
+                ) : (
+                  <AnchorLink anchor={link.anchor}>
+                    <span className="text-lg font-bold text-white" onClick={() => setMenuOpen(false)}>
+                      {link.name}
+                    </span>
+                  </AnchorLink>
+                )}
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <AnchorLink anchor="garys-goals">
+              <Button
+                className="my-1 h-12 border-2 border-transparent bg-gary-pink px-8 text-lg text-white shadow-md outline-none transition-all hover:border-gary-pink hover:bg-white hover:text-gary-pink dark:hover:bg-white dark:hover:text-gary-pink"
+                onClick={() => setMenuOpen(false)}
+              >
+                Buy $GARA coin
+              </Button>
+            </AnchorLink>
+          </div>
+          <div className="mt-8 flex flex-row gap-4">
+            <Button className="flex h-12 w-12 items-center justify-center rounded-full border-none bg-[#0D1E35] hover:!bg-gary-yellow">
+              <a href="https://www.facebook.com/profile.php?id=61568221981440">
+                <Image src={`/images/save-penguins/facebook.svg`} width={24} height={24} alt="Facebook" />
+              </a>
+            </Button>
+            <Button className="flex h-12 w-12 items-center justify-center rounded-full border-none bg-[#0D1E35] hover:!bg-gary-yellow">
+              <a href="https://x.com/Help_Gary_">
+                <Image src={`/images/save-penguins/twitter.svg`} width={24} height={24} alt="Twitter" />
+              </a>
+            </Button>
+            <Button className="flex h-12 w-12 items-center justify-center rounded-full border-none bg-[#0D1E35] hover:!bg-gary-yellow">
+              <a href="https://t.me/helpgary">
+                <Image src={`/images/save-penguins/telegram.svg`} width={24} height={24} alt="Telegram" />
+              </a>
+            </Button>
+          </div>
+        </div>
+      )}
     </>
   )
 }
